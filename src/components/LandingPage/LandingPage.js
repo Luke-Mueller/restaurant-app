f7c1e78e596923a969715baaaa997ecbab43ebf9
Luke Mueller <muelll79@hotmail.com>
2020-04-28 04:12:21 -0600

    converts a element to button element

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -12,9 +12,9 @@ const LandingPage = props => {
       <div className="LandingPage__div" />
       <h1 className="LandingPage__h1">{props.title}</h1>
       <p className="LandingPage__p">{props.text}</p>
-      <a className="LandingPage__a">
-        <span className="LandingPage__span" type="button">{props.btnValue}</span>
-      </a>
+      <button className="LandingPage__button">
+        <span className="LandingPage__span">{props.btnValue}</span>
+      </button>
     </div>
   );
 };
