97af529461ddc304add803be88caf83c6fdd9740
Luke Mueller <muelll79@hotmail.com>
2020-04-28 00:06:25 -0600

    creates components dir and LandingPage component skeleton

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="LandingPage">
+
+    </div>
+  );
+};
+
+export default LandingPage;
