0a3f138063586471c1775174b244031a87a45230
Luke Mueller <muelll79@hotmail.com>
2020-04-28 00:09:32 -0600

    imports LandingPage comp. and passes text prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
+import LandingPage from './components/LandingPage/LandingPage';
+
 import './App.css';
 
 function App() {
+  const text = 'Restaurant App';
+
   return (
     <div className="App">
-      
+      <LandingPage text={text} />
     </div>
   );
 }
