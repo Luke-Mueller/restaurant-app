5b4a46c218d8ec4e13ae8463b7f38570a3fde8e2
Luke Mueller <muelll79@hotmail.com>
2020-04-28 00:10:09 -0600

    adds img, div, and p to LandingPage comp

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import './LandingPage.css';
 
-const LandingPage = () => {
+const LandingPage = props => {
   return (
     <div className="LandingPage">
-
+      <img
+        className="Landing__img"
+        src="/images/logo.png"
+        alt="Spectrum Logo" />
+      <div className="Landing__div" />
+      <p className="Landing__p">{props.text}</p>
     </div>
   );
 };
