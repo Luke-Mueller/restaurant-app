892d9d7a59428b1e0a6a8dbe168dfabe1e1e29cc
Luke Mueller <muelll79@hotmail.com>
2020-04-28 14:26:38 -0600

    creates PageChangeAnimation component

---
+++ src/components/PageChangeAnimation/PageChangeAnimation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './PageChangeAnimation.css';
+
+const PageChangeAnimation = () => {
+  return (
+    <div className="PCA">
+        <div className="PCA__div-top" />
+        <div className="PCA__div-right" />
+        <div className="PCA__div-bottom" />
+        <div className="PCA__div-left" />
+    </div>
+  );
+};
+
+export default PageChangeAnimation;
