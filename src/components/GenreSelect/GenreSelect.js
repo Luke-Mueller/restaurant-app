c41b0ff1dd3357f4c77983fbdac5912bef4e4d0c
Luke Mueller <muelll79@hotmail.com>
2020-04-29 14:02:09 -0600

    completes GenreSelect comp

--- src/components/GenreSelect/GenreSelect.js
+++ src/components/GenreSelect/GenreSelect.js
@@ -2,6 +2,29 @@ import React from 'react';
 
 import './GenreSelect.css';
 
-const GenreSelect = () => {
-  
-}
+const GenreSelect = props => {
+  const genres = props.genresArr.map((genre, idx) => {
+    return (
+      <option
+        key={idx}
+        value={genre}
+        className="StatesSelect__option">
+        {genre}
+      </option>
+    );
+  });
+
+  return (
+    <select name="genres" className="GenreSelect">
+      <option
+        key="default"
+        value="default"
+        className="GenreSelect__option">
+        Filter by genre
+    </option>
+    {genres}
+    </select>
+  );
+};
+
+export default GenreSelect;
