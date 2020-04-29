e2d54b79827b9039e117cb8445aac26189e189fd
Luke Mueller <muelll79@hotmail.com>
2020-04-28 21:01:18 -0600

    sets background image

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import KEY from '../../utils/key';
 
 import './SearchPage.css';
 
+const URL = 'https://code-challenge.spectrumtoolbox.com/api/restaurants'
+
 const SearchPage = props => {
+  // const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(URL, {
+      headers: {
+        Authorization: KEY
+      }
+    })
+    .then(resp => resp.json())
+    .then(res => {
+      console.log('res: ', res)
+    })
+    .catch()
+
+  }, []);
+
   let classname = "SearchPage";
 
-  props.entered ? 
+  props.entered ?
     classname = "SearchPage active" :
     classname = "SearchPage"
   return (
     <div className={classname}>
-      <h1>Hello World</h1>
+
     </div>
   );
 };
