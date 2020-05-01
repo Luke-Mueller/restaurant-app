dd8311d5a38d0dacaa2c73ec7ae7ac200c766437
Luke Mueller <muelll79@hotmail.com>
2020-05-01 10:57:05 -0600

    turns states into objects

--- src/utils/states.js
+++ src/utils/states.js
@@ -1,54 +1,204 @@
 const States = [
-  "Alabama",
-  "Alaska",
-  "Arizona",
-  "Arkansas",
-  "California",
-  "Colorado",
-  "Connecticut",
-  "Delaware",
-  "District of Columbia",
-  "Florida",
-  "Georgia",
-  "Hawaii",
-  "Idaho",
-  "Illinois",
-  "Indiana",
-  "Iowa",
-  "Kansas",
-  "Kentucky",
-  "Louisiana",
-  "Maine",
-  "Maryland",
-  "Massachusettes",
-  "Michigan",
-  "Minnesota",
-  "Mississippi",
-  "Missouri",
-  "Montana",
-  "Nebraska",
-  "Nevada",
-  "New Hampshire",
-  "New Jersey",
-  "New Mexico",
-  "New York",
-  "North Carolina",
-  "North Dakota",
-  "Ohio",
-  "Oklahoma",
-  "Oregon",
-  "Pennsylvania",
-  "Rhode Island",
-  "South Dakota",
-  "Tennessee",
-  "Texas",
-  "Utah",
-  "Vermont",
-  "Virginia",
-  "Washington",
-  "West Virginia",
-  "Wisconsin",
-  "Wyoming"
+  {
+    name: "Alabama",
+    abb: "AL"
+  },
+  {
+    name: "Alaska",
+    abb: "AK"
+  },
+  {
+    name: "Arizona",
+    abb: "AZ"
+  },
+  {
+    name: "Arkansas",
+    abb: "AR"
+  },
+  {
+    name: "California",
+    abb: "CA"
+  },
+  {
+    name: "Colorado",
+    abb: "CO"
+  },
+  {
+    name: "Connecticut",
+    abb: "CT"
+  },
+  {
+    name: "Delaware",
+    abb: "DE"
+  },
+  {
+    name: "District of Columbia",
+    abb: "DC"
+  },
+  {
+    name: "Florida",
+    abb: "FL"
+  },
+  {
+    name: "Georgia",
+    abb: "GA"
+  },
+  {
+    name: "Hawaii",
+    abb: "HI"
+  },
+  {
+    name: "Idaho",
+    abb: "ID"
+  },
+  {
+    name: "Illinois",
+    abb: "IL"
+  },
+  {
+    name: "Indiana",
+    abb: "IN"
+  },
+  {
+    name: "Iowa",
+    abb: "IA"
+  },
+  {
+    name: "Kansas",
+    abb: "KS"
+  },
+  {
+    name: "Kentucky",
+    abb: "KY"
+  },
+  {
+    name: "Louisiana",
+    abb: "LA"
+  },
+  {
+    name: "Maine",
+    abb: "ME"
+  },
+  {
+    name: "Maryland",
+    abb: "MD"
+  },
+  {
+    name: "Massachusettes",
+    abb: "MA"
+  },
+  {
+    name: "Michigan",
+    abb: "MI"
+  },
+  {
+    name: "Minnesota",
+    abb: "MN"
+  },
+  {
+    name: "Mississippi",
+    abb: "MS"
+  },
+  {
+    name: "Missouri",
+    abb: "MO"
+  },
+  {
+    name: "Montana",
+    abb: "MT"
+  },
+  {
+    name: "Nebraska",
+    abb: "NE"
+  },
+  {
+    name: "Nevada",
+    abb: "NV"
+  },
+  {
+    name: "New Hampshire",
+    abb: "NH"
+  },
+  {
+    name: "New Jersey",
+    abb: "NJ"
+  },
+  {
+    name: "New Mexico",
+    abb: "NM"
+  },
+  {
+    name: "New York",
+    abb: "NY"
+  },
+  {
+    name: "North Carolina",
+    abb: "NC"
+  },
+  {
+    name: "North Dakota",
+    abb: "ND"
+  },
+  {
+    name: "Ohio",
+    abb: "OH"
+  },
+  {
+    name: "Oklahoma",
+    abb: "OK"
+  },
+  {
+    name: "Oregon",
+    abb: "OR"
+  },
+  {
+    name: "Pennsylvania",
+    abb: "PA"
+  },
+  {
+    name: "Rhode Island",
+    abb: "RI"
+  },
+  {
+    name: "South Dakota",
+    abb: "SD"
+  },
+  {
+    name: "Tennessee",
+    abb: "TN"
+  },
+  {
+    name: "Texas",
+    abb: "TX"
+  },
+  {
+    name: "Utah",
+    abb: "UT"
+  },
+  {
+    name: "Vermont",
+    abb: "VT"
+  },
+  {
+    name: "Virginia",
+    abb: "VA"
+  },
+  {
+    name: "Washington",
+    abb: "WA"
+  },
+  {
+    name: "West Virginia",
+    abb: "WV"
+  },
+  {
+    name: "Wisconsin",
+    abb: "WI"
+  },
+  {
+    name: "Wyoming",
+    abb: "WY"
+  }
 ];
 
 export default States;
