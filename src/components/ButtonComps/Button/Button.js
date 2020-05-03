8019c729d4dd21e2624d5dc5393a22947fd0bd3f
Luke Mueller <muelll79@hotmail.com>
2020-05-02 21:21:13 -0600

    adds props

--- src/components/ButtonComps/Button/Button.js
+++ src/components/ButtonComps/Button/Button.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Button = props => {
   return (
     <button
-      {...props}>
+      className={props.className}
+      onClick={props.onClick}
+      onMouseUp={props.onMouseUp}
+      type={props.type}
+      value={props.value}>
       <img
         className={props.imgClassName}
         src={props.imgSrc}
